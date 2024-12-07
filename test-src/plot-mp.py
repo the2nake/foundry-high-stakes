fd9aa6b5b9502c2010114cfc7faa2f6d129fad1d
the2nake <41810559+the2nake@users.noreply.github.com>
2024-12-07 17:41:31 +0800

    feat: combined position graphs

--- test-src/plot-mp.py
+++ test-src/plot-mp.py
@@ -19,7 +19,8 @@ def plot(ax, xs, ys, style="-"):
     (l,) = ax.plot(xs, ys, linestyle=style)
     return l
 
-fig, (ax_x, ax_y, ax_h, ax_vx, ax_vy, ax_vh) = plt.subplots(6)
+fig, ((ax_pos, ax_h), (ax_vx, ax_vh), (ax_vy, b)) = plt.subplots(3, 2)
+ax_pos.axis('equal')
 
 x_list_t, x_list = generate(lines, "x")
 y_list_t, y_list = generate(lines, "y")
@@ -30,16 +31,14 @@ vh_list_t, vh_list = generate(lines, "vh")
 
 # plot(ax, x_list, y_list)
 
-plot(ax_x, x_list_t, x_list)
-plot(ax_y, y_list_t, y_list)
+plot(ax_pos, x_list, y_list)
 plot(ax_h, h_list_t, h_list)
 
 plot(ax_vx, vx_list_t, vx_list)
 plot(ax_vy, vy_list_t, vy_list)
 plot(ax_vh, vh_list_t, vh_list)
 
-ax_x.grid()
-ax_y.grid()
+ax_pos.grid()
 ax_h.grid()
 ax_vx.grid()
 ax_vy.grid()
